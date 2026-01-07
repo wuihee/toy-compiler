f1844abad174540e17503294c179ac58ee0ee2c6
wuihee <wuihee@gmail.com>
2026-01-06 16:00:54 -0800

    feat (ast): implement ast nodes

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,35 @@
 //! # AST
 //!
 //! This module contains the data structures that make up the AST.
+
+/// Currently, an entire program consists of a list of statements.
+pub struct Program {
+    pub statements: Vec<Statement>,
+}
+
+/// A statement is a line of code. Currently we only have assignment.
+pub enum Statement {
+    Assignment { name: Identifier, value: Expression },
+}
+
+/// For now, an identifier is just a `String`.
+pub type Identifier = String;
+
+/// An `Expression` are the "units" of our language.
+pub enum Expression {
+    Binary {
+        left: Box<Expression>,
+        operator: Operator,
+        right: Box<Expression>,
+    },
+    Number(i64),
+    Variable(Identifier),
+}
+
+/// Possible operators we can use in our language.
+pub enum Operator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
