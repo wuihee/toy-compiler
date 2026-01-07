32d8bc41f1a5d26529c3d83422d345cc10a50a7d
wuihee <wuihee@gmail.com>
2026-01-06 16:33:17 -0800

    feat (parser): implement starting stuff

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,38 @@
+//! # Parser
+//!
+//! This module parses a stream of tokens and constructs an AST.
 
+use std::error::Error;
+
+use crate::{ast::Program, lexer::token::Token};
+
+/// Represents the current state of a parser.
+pub struct Parser {
+    /// A sequence of tokens representing a program.
+    tokens: Vec<Token>,
+
+    /// The position of the current token.
+    position: usize,
+}
+
+impl Parser {
+    /// Instantiate a new parser with a stream of tokens.
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Parser {
+            tokens,
+            position: 0,
+        }
+    }
+
+    /// Parse the input tokens into an AST.
+    ///
+    /// # Returns
+    ///
+    /// `Program` on success which represents the root of the AST, or `Error`
+    /// on failure if the syntax of the program is invalid.
+    pub fn parse(&mut self) -> Result<Program, Box<dyn Error>> {
+        Ok(Program {
+            statements: Vec::new(),
+        })
+    }
+}
