0f7ab80cfe4eadcf928c3f03a5b776765a1968c4
wuihee <wuihee@gmail.com>
2026-03-01 03:28:39 -0800

    feat(lexer): scaffolding

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,1 +1,15 @@
 //! Lexer
+//!
+//! This module is responsible for converting a string into a sequnce of tokens.
+
+use crate::lexer::token::Token;
+
+pub struct Lexer {
+    pub tokens: Vec<Token>,
+}
+
+impl Lexer {
+    pub fn new(tokens: Vec<Token>) -> Lexer {
+        Lexer { tokens }
+    }
+}
