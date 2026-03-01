74ca5e5b3967a4079fa8b1ff8e68ea6e6ccedb27
wuihee <wuihee@gmail.com>
2026-03-01 03:28:39 -0800

    feat(lexer): scaffolding

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,1 +1,9 @@
 //! Lexer
+//!
+//! This module is responsible for converting a given program into a sequnce
+//! of tokens.
+
+pub struct Lexer<'a> {
+    pub source: &'a str,
+    pub position: usize,
+}
