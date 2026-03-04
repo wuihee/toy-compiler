5d9a20121b52fc13614e87d27af3c283eca0a1e3
wuihee <wuihee@gmail.com>
2026-03-04 00:07:31 -0800

    feat(lexer): peek

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -8,13 +8,25 @@ use crate::lexer::{
     token::{Span, Token, TokenKind},
 };
 
+/// This struct converts a program into a stream of `Token`s.
 pub struct Lexer<'a> {
-    pub source: &'a str,
-    pub position: usize,
+    /// The source program as string.
+    source: &'a str,
+
+    /// A cursor into `source`.
+    position: usize,
 }
 
 // Correct lifetimes?
 impl<'a> Lexer<'a> {
+    /// Instantiate a new `Lexer`.
+    ///
+    /// # Examples
+    ///
+    /// ```rs
+    /// let source = "int x = 0;"
+    /// let lexer = Lexer::new(source);
+    /// ```
     pub fn new(source: &'a str) -> Lexer<'a> {
         Lexer {
             source,
@@ -22,10 +34,25 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    /// Pull the next token from the `source` program.
     pub fn next_token(&mut self) -> Result<Token, LexerError> {
+        if let Some(symbol) = self.peek() {
+            match symbol {
+                _ => {}
+            }
+        }
+
         Ok(Token {
             kind: TokenKind::Eof,
             span: Span { start: 0, end: 1 },
         })
+    }
+
+    /// Get the symbol at the current `position`.
+    fn peek(&self) -> Option<char> {
+        self.source
+            .as_bytes()
+            .get(self.position)
+            .map(|&symbol| symbol as char)
     }
 }
